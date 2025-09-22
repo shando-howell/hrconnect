d9d4d538bf5cd325060aff2521ffa45b0a3c7ff9
shandohowell <shandohowell94@gmail.com>
2025-09-21 20:06:52 -0500

    Modified eslint rules.

--- app/(signed-in)/layout.tsx
+++ app/(signed-in)/layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { AppSidebar } from "@/components/app-sidebar";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
 import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import Link from "next/link";
